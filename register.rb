09e63d134449fc7ff28d3a0a5d9422109e117050
yuto <nekopurin06300630@yahoo.co.jp>
2020-06-13 13:26:10 +0900

    ok

--- register.rb
+++ register.rb
@@ -1,4 +1,37 @@
-sum = 0
+class Sum
+  attr_accessor :sum, :thousand, :hundred, :ten, :one
+
+  def initialize(sum, thousand=0, hundred=0, ten=0, one=0)
+    @sum = sum
+    @thousand = thousand
+    @hundred = hundred
+    @ten = ten
+    @one = one
+  end
+
+  def confirm(number)
+    array = []
+    array = number.to_s.split("")
+    if array.length >= 4
+      @thousand = array[-4].to_i
+      @hundred = array[-3].to_i
+      @ten = array[-2].to_i
+      @one = array[-1].to_i
+    elsif array.length >= 3
+      @hundred = array[-3].to_i
+      @ten = array[-2].to_i
+      @one = array[-1].to_i
+    elsif array.length >= 2
+      @ten = array[-2].to_i
+      @one = array[-1].to_i
+    elsif array.length >= 2
+      @one = array[-1].to_i
+    end
+  end
+
+
+end
+
 loop{
  puts"合計金額を入力してください"
  sum = gets
@@ -6,24 +39,37 @@ loop{
  if sum == 0
    puts"値が間違っています"
  else
+   @money = Sum.new(sum)
    break
  end
 }
+@money.confirm(@money.sum)
+# @money
 
-money = 0
 loop{
  puts"金額を打ち込んでください"
- money = gets
- money = money.to_i
- if (money == 0) || (sum > money)
+ sum = gets
+ sum = sum.to_i
+  if (sum == 0) || (@money.sum > sum)
    puts"値が間違っています"
  else
+   @register = Sum.new(sum)
    break
  end
 }
-if (sum*0.001 >= 1)
- th = sum * 0.001
- th = th.floor
- th = th.to_s.split('')[-1]
- th = th.to_i
+@register.confirm(@register.sum)
+# @register
+
+if (@register.sum > @money.sum + 10000) #10000円
+  puts"値が間違っています"
+elsif (@register.thousand >= @money.thousand + 2) && (@register.thousand != 5)
+  puts"値が間違っています"
+elsif (@register.hundred >= @money.hundred + 2) && (@register.hundred != 5)
+  puts"値が間違っています"
+elsif (@register.ten >= @money.ten + 2) && (@register.ten != 5)
+  puts"値が間違っています"
+elsif (@register.one >= @money.one + 2) && (@register.one != 5)
+  puts"値が間違っています"
 end
+
+puts "お釣りは#{@register.sum - @money.sum}円です"
